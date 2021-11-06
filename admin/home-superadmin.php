13972a3dbd7966bc257c82dc629428f44b483165
Nics Projects <nicsprojects25@gmail.com>
2021-11-06 22:35:47 +0700

    it was born

---
+++ admin/home-superadmin.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, height=device-height, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
+        <link href="../img/logo-sponsor/icon-hitam.png" rel="icon">
+        <title>Superadmin [nama]</title>
+        <link href="../css/bootstrap-theme.css" rel="stylesheet">
+        <link href="../css/bootstrap.css" rel="stylesheet">
+        <link href="../css/skyler-admin.css" rel="stylesheet">
+        <script src="../js/jquery-2.1.4.js" type="text/javascript">
+        </script>
+        <script src="../js/skyler-admin.js" type="text/javascript">
+        </script>
+    </head>
+    <body>
+        <header>
+            <div class="in-header">
+                <div class="sub-in-header"></div>
+            </div>
+            <div class="in-header-bawah">
+                <ul>
+                    <li>menu 1</li>
+                    <li>menu 1</li>
+                    <li>menu 1</li>
+                    <li>menu 1</li>
+                    <li>menu 1</li>
+                </ul>
+            </div>
+        </header>
+        <div class="container"></div>
+        <footer>
+            <div class="in-footer">
+                <p>Copyright &copy; 2016 Skyler.com </p>
+            </div>
+        </footer>
+    </body>
+</html>
